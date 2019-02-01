bc99a834bb05e1abdbeb4ac31d1a7fb8f7011b66
Florin Mirica <miricaflorin@hotmail.com>
2019-02-01 14:41:44 -0800

    modified html and changed most of js

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,37 @@
-//Business Logic
 
-function Pizza(size) {
-  this.size = size;
-  this.toppings = [];
+function Pizza(amount, size, toppings) {
+    this.amount = amount;
+    this.size = size;
+    if (this.size === "large") {
+        this.basePrice = 10;
+    } else if (this.size === "extra large") {
+        this.basePrice = 12;
+    } else if (this.size === "medium") {
+        this.basePrice = 8;
+    } else if (this.size === "small") {
+        this.basePrice = 6;
+    } else {
+        this.basePrice = 15;
+    }
+
+
+    if (toppings != undefined) {
+        var allToppings = toppings;
+    } else {
+        allToppings = [];
+    }
+    this.toppings = allToppings;
 }
 
-Pizza.prototype.addTopping = function(topping) {
-  this.toppings.push(topping);
+Pizza.prototype.priceCalc = function() {
+    var pizzaBasePrice = this.basePrice;
+    var numberOfToppings = this.toppings.length;
+    var toppingsPrice = numberOfToppings * .75;
+    var amount = this.amount;
+    var totalCost = amount * (pizzaBasePrice + toppingsPrice);
+    return totalCost;
 }
 
-Pizza.prototype.cost = function() {
-  var cost = parseFloat(this.size) * 1;
-  cost += (this.toppings.length) * 0.5;
-  if (this.toppings.includes("Bacon")) {
-    cost += 0.5
-  }
-  return cost;
-}
 
-//User interface Logic
+
+});
