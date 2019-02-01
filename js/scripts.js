3b1924e466f4d1386e642a74cadeff3733140b1b
Florin Mirica <miricaflorin@hotmail.com>
2019-02-01 12:57:50 -0800

    create html with dropdown and checkboxes, add js functions for size and toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
-$(document).ready(function() {
-  jQuery('#btn1').click(function() {
-    alert("click");
-  });
-});
+//Business Logic
+
+function Pizza(size) {
+  this.size = size;
+  this.toppings = [];
+}
+
+Pizza.prototype.addTopping = function(topping) {
+  this.toppings.push(topping);
+}
+
+Pizza.prototype.cost = function() {
+  var cost = parseFloat(this.size) * 1;
+  cost += (this.toppings.length) * 0.5;
+  if (this.toppings.includes("Bacon")) {
+    cost += 0.5
+  }
+  return cost;
+}
+
+//User interface Logic
